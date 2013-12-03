fd03b27eeeca05da3ccecd656f65eaaff52afdaf
Bruno Pierri Galvao <galvaob2@Brunos-MacBook-Air.local>
2013-12-03 10:46:16 -0500

    has user signed up?

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      redirect_to signup_path
     end
   end
   
