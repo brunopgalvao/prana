f0caf32f9ab5a0e43e7d0e2ac8f0b3a54502b32d
Bruno Pierri Galvao <galvaob2@Brunos-MacBook-Air.local>
2013-12-02 21:19:46 -0500

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.configure do
+Prana::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
