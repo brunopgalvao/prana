47474c99355514fcfea1f236a462f4b2f526e8a6
Bruno Galvao <brunopgalvao@gmail.com>
2014-07-30 17:20:39 -0400

    fixed postgres issue of changing instructor column from string to integer

--- db/migrate/20140618225448_change_column_instructor_to_integer.rb
+++ db/migrate/20140618225448_change_column_instructor_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeColumnInstructorToInteger < ActiveRecord::Migration
   def change
-    change_column :yoga_classes, :instructor, :integer
+    change_column :yoga_classes, :instructor, 'integer USING CAST(instructor AS integer)'
   end
 end
